26984b2d5b330caefce04c9c063818c43de4f989
Nicovied0 <briannoviedo@gmail.com>
2023-12-04 11:53:21 -0300

    add more routes and method the pay

--- src/index.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const dbConnect = require("./src/config/mongo");
-const routes = require("./src/routes/index");
---
+++ src/routes/payment.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { createOrder, receiveWebhook } from "../controllers/payment.controller";
+
+const router = Router();
+
+router.post("/create-order", createOrder);
+
+router.post("/webhook", receiveWebhook);
+
+router.get("/success", (req, res) => res.send("Success"));
+
+export default router;
